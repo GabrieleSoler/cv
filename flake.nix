866536f1b5e2f83c1a3089181c877ecf50547ebf
Gabriele da Soler Vieira <gabriele.dasolervieira@gmail.com>
2026-08-04 20:50:52 -0300

    Sync flake.nix/lock from upstream; keep Gabriele_Vieira output names

--- flake.nix
+++ flake.nix
@@ -5,28 +5,36 @@
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
   };
 
-  outputs = { self, nixpkgs }:
-    let
-      pkgs = import nixpkgs { system = "x86_64-linux"; };
-    in
-    {
-      devShells.x86_64-linux.default = pkgs.mkShell {
-        packages = [ pkgs.texliveFull ];
-      };
-
-      packages.x86_64-linux.default = pkgs.stdenv.mkDerivation {
-        name = "cv";
-        src = ./.;
-        buildInputs = [ pkgs.texliveFull ];
-        buildPhase = ''
-          latexmk -pdf cv_english.tex
-          latexmk -pdf cv_portuguese.tex
-        '';
-        installPhase = ''
-          mkdir -p $out
-          cp cv_english.pdf $out/Gabriele_Vieira_Eng.pdf
-          cp cv_portuguese.pdf $out/Gabriele_Vieira_Por.pdf
-        '';
-      };
+  outputs = {
+    self,
+    nixpkgs,
+  }: let
+    pkgs = import nixpkgs {system = "x86_64-linux";};
+    texlive = pkgs.texliveSmall.withPackages (ps:
+      with ps; [
+        cm-super
+        geometry
+        latexmk
+        moderncv
+      ]);
+  in {
+    devShells.x86_64-linux.default = pkgs.mkShell {
+      packages = [texlive];
     };
+
+    packages.x86_64-linux.default = pkgs.stdenv.mkDerivation {
+      name = "cv";
+      src = ./.;
+      buildInputs = [texlive];
+      buildPhase = ''
+        latexmk -pdf cv_english.tex
+        latexmk -pdf cv_portuguese.tex
+      '';
+      installPhase = ''
+        mkdir -p $out
+        cp cv_english.pdf $out/Gabriele_Vieira_Eng.pdf
+        cp cv_portuguese.pdf $out/Gabriele_Vieira_Por.pdf
+      '';
+    };
+  };
 }
